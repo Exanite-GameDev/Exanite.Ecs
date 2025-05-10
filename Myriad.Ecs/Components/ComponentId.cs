2d4c8bb2ac57d86ece9fcfe139f428ab41efc2cb
William Chen <exanite78012@gmail.com>
2025-05-10 11:35:02 -0400

    Edit docs

--- Myriad.Ecs/Components/ComponentId.cs
+++ Myriad.Ecs/Components/ComponentId.cs
@@ -4,18 +4,18 @@ using System.Diagnostics;
 namespace Exanite.Myriad.Ecs.Components;
 
 /// <summary>
-/// Unique numeric ID for a type which implements IComponent
+/// Unique numeric ID for a type which implements IComponent.
 /// </summary>
 [DebuggerDisplay("{Type} ({Value})")]
 public readonly record struct ComponentId : IComparable<ComponentId>
 {
     /// <summary>
-    /// Get the raw value of this ID
+    /// Get the raw value of this ID.
     /// </summary>
     public int Value { get; }
 
     /// <summary>
-    /// The <see cref="System.Type"/> of the component this ID is for
+    /// The <see cref="System.Type"/> of the component this ID is for.
     /// </summary>
     public Type Type => ComponentRegistry.Get(this);
 
@@ -37,16 +37,16 @@ public readonly record struct ComponentId : IComparable<ComponentId>
     }
 
     /// <summary>
-    /// Get the component ID for the given type
+    /// Get the component ID for the given type.
     /// </summary>
-    /// <exception cref="ArgumentException">Thrown if 'type' does not implement <see cref="IComponent"/></exception>
+    /// <exception cref="ArgumentException">Thrown if <see cref="type"/> does not implement <see cref="IComponent"/>.</exception>
     public static ComponentId Get(Type type)
     {
         return ComponentRegistry.Get(type);
     }
 
     /// <summary>
-    /// Get the component ID for the given type
+    /// Get the component ID for the given type.
     /// </summary>
     public static ComponentId Get<T>() where T : IComponent
     {
