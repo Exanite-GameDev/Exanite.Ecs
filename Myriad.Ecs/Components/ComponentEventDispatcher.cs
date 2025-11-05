95ceb3fbbd580ae32030ab5c0602dd903925c3bd
William Chen <exanite78012@gmail.com>
2025-11-05 00:00:19 -0500

    Fix component events being ran on copies of the components instead of the ref

--- Myriad.Ecs/Components/ComponentEventDispatcher.cs
+++ Myriad.Ecs/Components/ComponentEventDispatcher.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using Exanite.Myriad.Ecs.CommandBuffers;
 using Exanite.Myriad.Ecs.Events;
 
@@ -9,25 +10,83 @@ internal abstract class ComponentEventDispatcher
     public abstract void OnComponentAdded(EcsCommandBuffer recursiveCommandBuffer, EcsWorld world, Entity entity);
     public abstract void OnComponentModified(EcsCommandBuffer recursiveCommandBuffer, EcsWorld world, Entity entity);
     public abstract void OnComponentRemoved(EcsCommandBuffer recursiveCommandBuffer, EcsWorld world, Entity entity);
+
+    internal static void SetSelf<T>(ref T component, Entity entity) where T : IComponent, IComponentSelfReference<T>
+    {
+        component.Self = entity.GetStorableComponent<T>();
+    }
+
+    internal static void Setup<T>(ref T component) where T : IComponent, IComponentSetup
+    {
+        component.Setup();
+    }
+
+    internal static void Teardown<T>(ref T component) where T : IComponent, IComponentTeardown
+    {
+        component.Teardown();
+    }
+
+    internal static void Dispose<T>(ref T component) where T : IComponent, IDisposable
+    {
+        component.Dispose();
+    }
 }
 
 internal class ComponentEventDispatcher<T> : ComponentEventDispatcher where T : IComponent
 {
+    private delegate void SetSelfAction(ref T component, Entity entity);
+    private delegate void SetupAction(ref T component);
+    private delegate void TeardownAction(ref T component);
+    private delegate void DisposeAction(ref T component);
+
+    private readonly SetSelfAction? setSelf;
+    private readonly SetupAction? setup;
+    private readonly TeardownAction? teardown;
+    private readonly DisposeAction? dispose;
+
+    public ComponentEventDispatcher()
+    {
+        if (typeof(T).IsAssignableTo(typeof(IComponentSelfReference<T>)))
+        {
+            setSelf = (SetSelfAction)typeof(ComponentEventDispatcher).GetMethod(nameof(SetSelf), BindingFlags.NonPublic | BindingFlags.Static)!
+                .MakeGenericMethod(typeof(T))
+                .CreateDelegate(typeof(SetSelfAction), null);
+        }
+
+        if (typeof(T).IsAssignableTo(typeof(IComponentSetup)))
+        {
+            setup = (SetupAction)typeof(ComponentEventDispatcher).GetMethod(nameof(Setup), BindingFlags.NonPublic | BindingFlags.Static)!
+                .MakeGenericMethod(typeof(T))
+                .CreateDelegate(typeof(SetupAction), null);
+        }
+
+        if (typeof(T).IsAssignableTo(typeof(IComponentTeardown)))
+        {
+            teardown = (TeardownAction)typeof(ComponentEventDispatcher).GetMethod(nameof(Teardown), BindingFlags.NonPublic | BindingFlags.Static)!
+                .MakeGenericMethod(typeof(T))
+                .CreateDelegate(typeof(TeardownAction), null);
+        }
+
+        if (typeof(T).IsAssignableTo(typeof(IDisposable)))
+        {
+            dispose = (DisposeAction)typeof(ComponentEventDispatcher).GetMethod(nameof(Dispose), BindingFlags.NonPublic | BindingFlags.Static)!
+                .MakeGenericMethod(typeof(T))
+                .CreateDelegate(typeof(DisposeAction), null);
+        }
+    }
+
     public override void OnComponentAdded(EcsCommandBuffer recursiveCommandBuffer, EcsWorld world, Entity entity)
     {
         ref var component = ref entity.GetComponent<T>();
 
-        // ReSharper disable once MergeCastWithTypeCheck
         if (component is IComponentSelfReference<T>)
         {
-            ((IComponentSelfReference<T>)component).Self = entity.GetStorableComponent<T>();
+            setSelf!.Invoke(ref component, entity);
         }
 
-        // ReSharper disable once MergeCastWithTypeCheck
-        // ReSharper disable once SuspiciousTypeConversion.Global
         if (component is IComponentSetup)
         {
-            ((IComponentSetup)component).Setup();
+            setup!.Invoke(ref component);
         }
 
         world.EventBus.Raise(new ComponentAdded<T>(recursiveCommandBuffer, entity, ref component));
@@ -44,17 +103,14 @@ internal class ComponentEventDispatcher<T> : ComponentEventDispatcher where T : 
         ref var component = ref entity.GetComponent<T>();
         world.EventBus.Raise(new ComponentRemoved<T>(recursiveCommandBuffer, entity, ref component));
 
-        // ReSharper disable once MergeCastWithTypeCheck
-        // ReSharper disable once SuspiciousTypeConversion.Global
         if (component is IComponentTeardown)
         {
-            ((IComponentTeardown)component).Teardown();
+            teardown!.Invoke(ref component);
         }
 
-        // ReSharper disable once MergeCastWithTypeCheck
         if (component is IDisposable)
         {
-            ((IDisposable)component).Dispose();
+            dispose!.Invoke(ref component);
         }
     }
 }
