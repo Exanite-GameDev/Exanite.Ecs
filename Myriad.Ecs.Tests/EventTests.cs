0d55dc8842a95553a28badb315567c6d6db14ed0
William Chen <exanite78012@gmail.com>
2025-11-05 01:59:53 -0500

    Add new event tests to cover disposing functionality

--- Myriad.Ecs.Tests/EventTests.cs
+++ Myriad.Ecs.Tests/EventTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Exanite.Core.Events;
 using Exanite.Core.Runtime;
 using Exanite.Core.Utilities;
@@ -467,6 +468,97 @@ public class EventTests
 
         world.Dispose();
         Assert.Throws<GuardException>(() => commandBuffer.Execute());
+    }
+
+    [Test]
+    public void ComponentDisposable_IsDisposed_WhenDestroyed()
+    {
+        var world = new EcsWorld();
+        var commandBuffer = world.AcquireCommandBuffer();
+
+        var disposeCount = 0;
+        var bufferedEntity = commandBuffer.Create()
+            .Set(new ComponentDisposable()
+            {
+                DisposeAction = () => disposeCount++,
+            });
+
+        commandBuffer.Execute();
+        Assert.That(disposeCount, Is.EqualTo(0));
+
+        var entity = bufferedEntity.Resolve();
+        commandBuffer.Destroy(entity);
+
+        commandBuffer.Execute();
+        Assert.That(disposeCount, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void ComponentDisposable_IsDisposed_WhenOverwritten_InCommandBuffer()
+    {
+        var world = new EcsWorld();
+        var commandBuffer = world.AcquireCommandBuffer();
+
+        var disposeCount = 0;
+        var bufferedEntity = commandBuffer.Create()
+            .Set(new ComponentDisposable()
+            {
+                DisposeAction = () => disposeCount++,
+            });
+        Assert.That(disposeCount, Is.EqualTo(0));
+
+        bufferedEntity.Set(new ComponentDisposable()
+        {
+            DisposeAction = () => disposeCount++,
+        });
+        Assert.That(disposeCount, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void ComponentDisposable_IsDisposed_WhenCommandBuffer_IsCleared()
+    {
+        var world = new EcsWorld();
+        var commandBuffer = world.AcquireCommandBuffer();
+
+        var disposeCount = 0;
+        commandBuffer.Create()
+            .Set(new ComponentDisposable()
+            {
+                DisposeAction = () => disposeCount++,
+            });
+        Assert.That(disposeCount, Is.EqualTo(0));
+
+        commandBuffer.Clear();
+        Assert.That(disposeCount, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void DisposingWorld_ClearsCommandBuffer()
+    {
+        var world = new EcsWorld();
+        var commandBuffer = world.AcquireCommandBuffer();
+
+        var disposeCount = 0;
+        commandBuffer.Create()
+            .Set(new ComponentDisposable()
+            {
+                DisposeAction = () => disposeCount++,
+            });
+        Assert.That(disposeCount, Is.EqualTo(0));
+
+        world.Dispose();
+        Assert.That(commandBuffer.HasBufferedOperations, Is.False);
+        Assert.That(disposeCount, Is.EqualTo(1));
+    }
+
+    private struct ComponentDisposable : IComponent, IDisposable
+    {
+        public required Action DisposeAction;
+
+        public void Dispose()
+        {
+            DisposeAction.Invoke();
+        }
     }
 
     private class WorldEventHandler :
