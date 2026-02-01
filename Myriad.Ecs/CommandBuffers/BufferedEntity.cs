732be00b02766aa47c34adb80bd680dc7183684e
William Chen <exanite78012@gmail.com>
2026-02-01 03:54:31 -0500

    Change BufferedEntity to be a ref struct

--- Myriad.Ecs/CommandBuffers/BufferedEntity.cs
+++ Myriad.Ecs/CommandBuffers/BufferedEntity.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace Exanite.Myriad.Ecs.CommandBuffers;
+﻿namespace Exanite.Myriad.Ecs.CommandBuffers;
 
 /// <summary>
 /// An entity that is being processed by a command buffer.
 /// Mainly used for fluent method chaining.
 /// </summary>
-public readonly record struct BufferedEntity : IEquatable<Entity>
+public readonly ref struct BufferedEntity
 {
     public readonly Entity Entity;
     public readonly EcsCommandBuffer CommandBuffer;
@@ -22,12 +20,6 @@ public readonly record struct BufferedEntity : IEquatable<Entity>
         return value.Entity;
     }
 
-    public static bool operator ==(BufferedEntity a, Entity b) => a.Entity == b;
-    public static bool operator !=(BufferedEntity a, Entity b) => a.Entity != b;
-
-    public static bool operator ==(Entity a, BufferedEntity b) => a == b.Entity;
-    public static bool operator !=(Entity a, BufferedEntity b) => a != b.Entity;
-
     /// <inheritdoc cref="EcsCommandBuffer.Set"/>
     public BufferedEntity Set<T>(T value) where T : IComponent
     {
@@ -38,15 +30,5 @@ public readonly record struct BufferedEntity : IEquatable<Entity>
     public BufferedEntity Remove<T>() where T : IComponent
     {
         return CommandBuffer.Remove<T>(Entity);
-    }
-
-    public bool Equals(Entity other)
-    {
-        return Entity.Equals(other);
-    }
-
-    public override int GetHashCode()
-    {
-        return Entity.GetHashCode();
     }
 }
