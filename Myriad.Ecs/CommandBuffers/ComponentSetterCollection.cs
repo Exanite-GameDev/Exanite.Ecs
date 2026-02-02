9618aab4b55e874a4e7e79a248538129a06b5d80
William Chen <exanite78012@gmail.com>
2026-02-01 23:47:02 -0500

    Cleanup

--- Myriad.Ecs/CommandBuffers/ComponentSetterCollection.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using System.Collections.Generic;
-using Exanite.Core.Pooling;
-using Exanite.Myriad.Ecs.Components;
-using Exanite.Myriad.Ecs.Worlds;
-
-namespace Exanite.Myriad.Ecs.CommandBuffers;
