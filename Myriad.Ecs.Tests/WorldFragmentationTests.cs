31a887050d9985be91a7cbbd01d94ff1be004063
William Chen <exanite78012@gmail.com>
2026-02-04 23:21:06 -0500

    Add test: CreatingMaxPlusOneEntities_ThenDestroyingFirst_FragmentsChunks

--- Myriad.Ecs.Tests/WorldFragmentationTests.cs
+++ Myriad.Ecs.Tests/WorldFragmentationTests.cs
@@ -63,4 +63,44 @@ public class WorldFragmentationTests
         Assert.Equal(1, world.Archetypes.Length);
         Assert.Equal(0, world.Archetypes[0].Chunks.Length);
     }
+
+    [Fact]
+    // This documents that destroying entities can leave partially filled chunks
+    // and ensures that manual compaction works
+    //
+    // Whether destroying entities should leave partially filled chunks is another question,
+    // but not needing to get the last entity from the last chunk during entity destruction simplifies things
+    public void CreatingMaxPlusOneEntities_ThenDestroyingFirst_FragmentsChunks()
+    {
+        var world = new EcsWorld();
+        using var _ = world.AcquireCommandBuffer(out var commandBuffer);
+
+        var first = commandBuffer.Create();
+        for (var i = 0; i < EcsConstants.ChunkEntityCount; i++)
+        {
+            commandBuffer.Create();
+        }
+
+        commandBuffer.Execute();
+
+        // Should be two chunks
+        Assert.Equal(EcsConstants.ChunkEntityCount + 1, world.Count());
+        Assert.Equal(1, world.Archetypes.Length);
+        Assert.Equal(2, world.Archetypes[0].Chunks.Length);
+
+        commandBuffer.Destroy(first);
+        commandBuffer.Execute();
+
+        // Should still be two chunks
+        Assert.Equal(EcsConstants.ChunkEntityCount, world.Count());
+        Assert.Equal(1, world.Archetypes.Length);
+        Assert.Equal(2, world.Archetypes[0].Chunks.Length);
+
+        world.Compact();
+
+        // Now we should have one chunk
+        Assert.Equal(EcsConstants.ChunkEntityCount, world.Count());
+        Assert.Equal(1, world.Archetypes.Length);
+        Assert.Equal(1, world.Archetypes[0].Chunks.Length);
+    }
 }
