076867f909d47ecffc88c9b99db34ae6b7f8ec2a
William Chen <exanite78012@gmail.com>
2026-02-11 07:51:54 -0500

    Fix 0 being used as an archetype ID
    (cherry picked from commit fd9b09afa83ffa2c380c9cdfe7153f1a27ece144)

--- Myriad.Ecs/EcsWorld.cs
+++ Myriad.Ecs/EcsWorld.cs
@@ -282,7 +282,7 @@ public sealed class EcsWorld : IArchetypeView, ITrackedDisposable
         }
 
         // Didn't find one, create the new archetype
-        var newArchetype = new Archetype(archetypes.Count, this, components.ToImmutableOrderedListSet());
+        var newArchetype = new Archetype(archetypes.Count + 1, this, components.ToImmutableOrderedListSet());
 
         // Add it to the relevant lists
         archetypes.Add(newArchetype);
